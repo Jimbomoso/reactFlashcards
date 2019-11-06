44ef4095f5063919e2b02432cfea50f3408ba390
jimbomoso <jimbomoso@gmail.com>
2019-11-06 16:19:46 -0500

    removed db

--- src/config/firebase/db_config.js
+++ src/config/firebase/db_config.js
@@ -1,9 +1,1 @@
-export const DB_CONFIG = {
-    apiKey: "AIzaSyC7FtvJ3vhiaMTKpvq7IL0TBY6Vf93-bwY",
-    authDomain: "flashcards-a6a95.firebaseapp.com",
-    databaseURL: "https://flashcards-a6a95.firebaseio.com",
-    projectId: "flashcards-a6a95",
-    storageBucket: "flashcards-a6a95.appspot.com",
-    messagingSenderId: "660558269764",
-    appId: "1:660558269764:web:7fd207ac65c9578bafdb11"
-};
+
